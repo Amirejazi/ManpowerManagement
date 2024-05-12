c69434d76651c82a5c9c55f3779401e57d0b685f
Amirhosein <ejaziamirhosein@gmail.com>
2024-05-12 22:21:11 +0330

    Mapping Profile

--- MP_Management.Application/Profile/MappingProfile.cs
+++ MP_Management.Application/Profile/MappingProfile.cs
@@ -9,10 +9,25 @@ namespace MP_Management.Profile
 	{
 		public MappingProfile()
 		{
-			CreateMap<LeaveType, LeaveTypeDTO>().ReverseMap();
-			CreateMap<LeaveRequest, LeaveRequestDTO>().ReverseMap();
-			CreateMap<LeaveRequest, LeaveRequestListDTO>().ReverseMap();
-			CreateMap<LeaveAllocation, LeaveAllocationDTO>().ReverseMap();
+			#region LeaveType Mapping
+				CreateMap<LeaveType, LeaveTypeDTO>().ReverseMap();
+				CreateMap<LeaveType, CreateLeaveTypeDTO>().ReverseMap();
+				CreateMap<LeaveType, UpdateLeaveTypeDTO>().ReverseMap();
+			#endregion
+
+			#region LeaveRequest Mapping
+				CreateMap<LeaveRequest, LeaveRequestListDTO>().ReverseMap();
+				CreateMap<LeaveRequest, LeaveRequestDTO>().ReverseMap();
+				CreateMap<LeaveRequest, CreateLeaveRequestDTO>().ReverseMap();
+				CreateMap<LeaveRequest, UpdateLeaveRequestDTO>().ReverseMap();
+			#endregion
+
+			#region LeaveAllocation Mapping
+				CreateMap<LeaveAllocation, LeaveAllocationDTO>().ReverseMap();
+				CreateMap<LeaveAllocation, CreateLeaveAllocationDTO>().ReverseMap();
+				CreateMap<LeaveAllocation, UpdateLeaveAllocationDTO>().ReverseMap();
+			#endregion
+
 		}
 	}
 }
