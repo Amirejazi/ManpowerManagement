ab8aed01327f28709403af5ee015fed9d214a2e9
Amirhosein <ejaziamirhosein@gmail.com>
2024-05-12 22:20:49 +0330

    --

--- MP_Management.Application/Persistence/Contracts/ILeaveRequestRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using MP_Management.Domain;
-
-namespace MP_Management.Persistence.Contracts
-{
-	public interface ILeaveRequestRepository: IGenericRepository<LeaveRequest>
-	{
-		Task ChangeApprovalStatus(LeaveRequest leaveRequest, bool? approvalStatus);
-	}
-}
