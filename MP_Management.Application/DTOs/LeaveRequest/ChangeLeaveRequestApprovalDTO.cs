a31511019780dddb180e528a4929810064aad053
Amirhosein <ejaziamirhosein@gmail.com>
2024-05-16 01:30:17 +0330

    update validators

--- MP_Management.Application/DTOs/LeaveRequest/ChangeLeaveRequestApprovalDTO.cs
+++ MP_Management.Application/DTOs/LeaveRequest/ChangeLeaveRequestApprovalDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MP_Management.Application.DTOs.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace MP_Management.Application.DTOs.LeaveRequest
 {
-	public class ChangeLeaveRequestApprovalDTO
+	public class ChangeLeaveRequestApprovalDTO: BaseDTO
 	{
 		public bool Approved { get; set; }
 	}
