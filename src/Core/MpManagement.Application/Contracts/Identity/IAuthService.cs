2ac1330ac4dd2ecad34afb562bc48848da2097ba
Amirhosein <ejaziamirhosein@gmail.com>
2024-05-22 11:21:55 +0330

    add identity models in application

---
+++ src/Core/MpManagement.Application/Contracts/Identity/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using MpManagement.Application.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MpManagement.Application.Contracts.Identity
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> Login(AuthResponse authResponse);
+        Task<RegisterationResponse> Register(RegisterationRequest registerationRequest);
+
+    }
+}
